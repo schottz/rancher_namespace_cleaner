34c2268567cc0e9e243c558d004981266c7e811e
Felipe Schottz <felipe.schottz@memora.com.br>
2024-02-05 13:20:42 -0300

    first commit

--- namespace_eraser.sh
+++ namespace_eraser.sh
@@ -8,8 +8,8 @@
 
 rancher_namespaces=$(kubectl get ns -o=custom-columns=:metadata.name | grep 'cattle\|p-\|local\|fleet\|user-' )
 
-for ns in $rancher_namespaces; do
-    kubectl delete $ns --force
+for namespace in $rancher_namespaces; do
+    kubectl delete ns $namespace --force
+    kubectl get ns $namespace -o json | jq 'del(.metadata.finalizers)' | jq '.spec = {}' > tempfile.json
+    kubectl replace --raw /api/v1/namespaces/$namespace/finalize -f tempfile.json
 done
-
-python3 cleaner.py
